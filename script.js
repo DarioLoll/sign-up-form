0e3acc95404eca569ca9a84207bfb108d623c804
DarioLoll <stromajer.dario@gmail.com>
2024-09-29 13:21:44 +0200

    Add validations and styles for them

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById("sign-up-form").addEventListener("submit", function(event) {
+    // Prevent the form from submitting and reloading the page
+    event.preventDefault();
+    alert("Form submitted!");
+  });
